c0009e53cd64dfb9159366f256762a0a08982498
Eugene Zamriy <ezamriy@cloudlinux.com>
2016-04-26 14:05:07 +0300

    Initial upload of the CentOS 7.2 x86_64 OpenNebula template.

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 PACKER = /usr/local/bin/packer
 
-all: centos-6-i386.opennebula centos-6-x86_64.opennebula
+all: centos-6-i386.opennebula centos-6-x86_64.opennebula centos-7-x86_64.opennebula
 
 centos-6-i386.opennebula:
 	$(PACKER) build -only centos-6-i386.opennebula centos-6-i386.opennebula.json
 
 centos-6-x86_64.opennebula:
 	$(PACKER) build -only centos-6-x86_64.opennebula centos-6-x86_64.opennebula.json
+
+centos-7-x86_64.opennebula:
+	$(PACKER) build -only centos-7-x86_64.opennebula centos-7-x86_64.opennebula.json
 
 clean:
 	rm -fr builds
--- provisioners/opennebula.sh
+++ provisioners/opennebula.sh
@@ -5,11 +5,18 @@
 # description: OpenNebula virtual machine configuration.
 
 
+DISTR_VERSION=$(python -c 'import platform; print platform.dist()[1].split(".")[0]')
+
 # setup contextualization tools
 yum install -y https://github.com/OpenNebula/addon-context-linux/releases/download/v4.14.4/one-context_4.14.4.rpm
 
 # setup cloud-utils-growpart for an automated root partition resizing
-yum install -y cloud-utils-growpart parted dracut-modules-growroot --enablerepo=epel
+if [ "${DISTR_VERSION}" -eq "7" ]; then
+  # NOTE: dracut-modules-growroot is not needed on EL7
+  yum install -y cloud-utils-growpart parted --enablerepo=epel
+else
+  yum install -y cloud-utils-growpart parted dracut-modules-growroot --enablerepo=epel
+fi
 
 # update initramfs
 dracut -f
