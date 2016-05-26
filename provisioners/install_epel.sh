2fde6bae27186665d1deb88f4a1698ca37870e12
Eugene Zamriy <ezamriy@cloudlinux.com>
2016-05-26 11:03:31 +0300

    Install epel-release package from CloudLinux channels when it's possible

--- provisioners/install_epel.sh
+++ provisioners/install_epel.sh
@@ -5,8 +5,11 @@
 # description: adds EPEL repository and disables it by default.
 
 
-DISTR_VERSION=$(python -c 'import platform; print platform.dist()[1].split(".")[0]')
+yum install -y epel-release
 
-yum install -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-${DISTR_VERSION}.noarch.rpm
+if [ $? -ne 0 ]; then
+  DISTR_VERSION=$(python -c 'import platform; print platform.dist()[1].split(".")[0]')
+  yum install -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-${DISTR_VERSION}.noarch.rpm
+fi
 
 sed -i -e '/\[epel\]/,/^\[/s/enabled=1/enabled=0/' /etc/yum.repos.d/epel.repo
