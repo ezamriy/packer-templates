8137ea6c4539ffb891010ed818d22488b8aae908
Eugene Zamriy <ezamriy@cloudlinux.com>
2016-05-29 19:57:03 +0300

    CloudLinux 5.11 OpenNebula templates initial upload.

--- Makefile
+++ Makefile
@@ -2,12 +2,23 @@ CL_ACTIVATION_KEY = undefined
 PACKER = /usr/local/bin/packer
 
 
+cloudlinux-5.11: cloudlinux-5.11-x86_64.opennebula \
+	               cloudlinux-5.11-i386.opennebula
+
 cloudlinux-6.8: cloudlinux-6.8-x86_64.opennebula \
 	              cloudlinux-6.8-i386.opennebula \
 								cloudlinux-6.8-x86_64.vagrant \
 								cloudlinux-6.8-i386.vagrant
 
 cloudlinux-7.2: cloudlinux-7.2-x86_64.vagrant cloudlinux-7.2-x86_64.opennebula
+
+cloudlinux-5.11-x86_64.opennebula:
+	$(PACKER) build -var CL_ACTIVATION_KEY=$(CL_ACTIVATION_KEY) \
+                  -only cloudlinux-5.11-x86_64.opennebula cloudlinux-5.11.json
+
+cloudlinux-5.11-i386.opennebula:
+	$(PACKER) build -var CL_ACTIVATION_KEY=$(CL_ACTIVATION_KEY) \
+                  -only cloudlinux-5.11-i386.opennebula cloudlinux-5.11.json
 
 cloudlinux-6.8-x86_64.opennebula:
 	$(PACKER) build -var CL_ACTIVATION_KEY=$(CL_ACTIVATION_KEY) \
--- provisioners/opennebula.sh
+++ provisioners/opennebula.sh
@@ -7,19 +7,28 @@
 
 DISTR_VERSION=$(python -c 'import platform; print platform.dist()[1].split(".")[0]')
 
-# setup contextualization tools
-yum install -y https://github.com/OpenNebula/addon-context-linux/releases/download/v4.14.4/one-context_4.14.4.rpm
+CONTEXT_PKG='one-context_4.14.4.rpm'
+CONTEXT_PKG_URL="https://github.com/OpenNebula/addon-context-linux/releases/download/v4.14.4/${CONTEXT_PKG}"
 
-# setup cloud-utils-growpart for an automated root partition resizing
-if [ "${DISTR_VERSION}" -eq "7" ]; then
-  # NOTE: dracut-modules-growroot is not needed on EL7
-  yum install -y cloud-utils-growpart parted --enablerepo=epel
+
+if [ "${DISTR_VERSION}" -eq "5" ]; then
+  curl -L -O "${CONTEXT_PKG_URL}"
+  rpm -Uvh "${CONTEXT_PKG}"
+  rm -f "${CONTEXT_PKG}"
+
+  sed -i -e 's/CONTEXT_DEV=`blkid -l -t LABEL="CONTEXT" -o device`/CONTEXT_DEV="\/dev\/cdrom"/' /usr/sbin/one-contextd
+  sed -i -e 's/-L CONTEXT -o ro/-o ro ${CONTEXT_DEV}/' /usr/sbin/one-contextd
 else
-  yum install -y cloud-utils-growpart parted dracut-modules-growroot --enablerepo=epel
+  yum install -y "${CONTEXT_PKG_URL}"
+  if [ "${DISTR_VERSION}" -eq "7" ]; then
+    yum install -y cloud-utils-growpart parted --enablerepo=epel
+  else
+    yum install -y cloud-utils-growpart parted dracut-modules-growroot --enablerepo=epel
+  fi
+  # update initramfs
+  dracut -f
 fi
 
-# update initramfs
-dracut -f
 
 # delete root password
 passwd -d root
